ead1d55b3d0f7bbc4d0a7b07d288a4f678fcfed1
Oleksiy Ostapenko <oleksiy.ostapenko@scsynergy.com>
2017-02-20 18:40:33 +0100

    project "Use jQuery to Fetch and Show Code School Badges Using Ajax" completion

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
 $(function() {
 
-  // your code will go here
+    $.ajax({
+    url: 'https://www.codeschool.com/users/oleksaostapenko.json',
+    dataType: 'jsonp',
+    success: function(response) {
+    	var courseContainer = $.map(response.courses.completed, function(course, index){
+            var item = $('<div class="course"></div>');
+            $('<h3>' + course.title + '</h3>').appendTo(item);
+            $('<img>').attr('src', course.badge).appendTo(item);
+    		$('<a>See Course</a>').attr({'href': course.url, 'target': '_blank', 'class': 'btn btn-primary'}).appendTo(item);
+            return item;
+    	});
+            
+        $('#badges').html(courseContainer);
+    }
+  });
 
 });
